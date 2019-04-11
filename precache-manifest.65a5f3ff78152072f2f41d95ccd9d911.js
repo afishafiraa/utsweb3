90267be040ef207ee0b05aaefcb8d3323a970d6f
Shafira Ananda <afishafiraa@gmail.com>
2019-04-11 09:29:20 +0700

    Updates

---
+++ precache-manifest.65a5f3ff78152072f2f41d95ccd9d911.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/utsweb3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "371dd3676839bd68e7ae",
+    "url": "/utsweb3/static/js/runtime~main.0bfe0192.js"
+  },
+  {
+    "revision": "350eaaa5578cdf834908",
+    "url": "/utsweb3/static/js/main.26c755d2.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/utsweb3/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "350eaaa5578cdf834908",
+    "url": "/utsweb3/static/css/main.ee343ee1.chunk.css"
+  },
+  {
+    "revision": "7b35f315255d19191a52f7fde123973e",
+    "url": "/utsweb3/index.html"
+  }
+];
